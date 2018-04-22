938c371d890127822d958e8cfe7c1b36b071adc0
masataka <ar.tec.masa@gmail.com>
2018-04-22 22:56:39 +0900

    html読み込み

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
 var http = require('http');
+    fs = require('fs')
 var settings = require('./settings');
-console.log(settings);
 var server = http.createServer();
 server.on('request', function(req, res){
-    switch (req.url) {
-        case '/about':
-            msg = "about title page";
-            break;
-        case '/profile':
-            msg = "about me";
-            break;
-        default:
-            msg = 'wrong page';
-            break;
-    }
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(msg);
-    res.end();
+    fs.readFile(__dirname + '/public_html/hello.html', 'utf-8', function(err, data){
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.write("not found!");
+            return res.end();
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    });
 });
 server.listen(settings.port);
 console.log("server listening....")
