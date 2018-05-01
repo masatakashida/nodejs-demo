74819e31eb8c49e81e6ff727dc4473c9cfcd6b01
masataka <ar.tec.masa@gmail.com>
2018-05-01 23:10:27 +0900

    fix

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
 var http = require('http'),
     fs = require('fs'),
-    ejs = require('ejs');
+    ejs = require('ejs'),
+    qs = require('querystring');
 var settings = require('./settings');
 var server = http.createServer();
-var template = fs.readFileSync(__dirname + '/public_html/hello.ejs', 'utf-8');
-var n = 0;
-server.on('request', function(req, res){
-    n++
+var template = fs.readFileSync(__dirname + '/public_html/bbs.ejs', 'utf-8');
+var posts = [];
+function renderForm(posts, res) {
     var data = ejs.render(template, {
-        title: "hello",
-        content: "<strong>World!</strong>",
-        n: n
+        posts: posts
     });
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     res.end();
+}
+server.on('request', function(req, res){
+    if (req.method === 'POST') {
+        req.data = "";
+        req.on("readable", function() {
+            req.data += req.read();
+        });
+        req.on("end", function() {
+            var query = qs.parse(req.data);
+            posts.push(query.name);
+            renderFrom(posts, res);
+        });
+    } else {
+        renderForm(posts, res)
+    }
 });
 server.listen(settings.port);
 console.log("server listening....")
